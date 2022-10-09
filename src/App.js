33e88864c101a2bbb7a07e929c60e0538f875b6e
Astrid-cenT <iskycruise01@gmail.com>
2022-10-09 11:48:10 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        {/* <Footer /> */}
+        <Footer />
       </BrowserRouter>
     </>
   );
