e0390e29cc7788307c931246cd87a22a6ebd5056
Astrid-cenT <iskycruise01@gmail.com>
2022-10-09 11:57:45 +0700

    up

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Head />
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" exact element={<Home />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
